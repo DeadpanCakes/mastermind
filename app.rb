9086681e7852a78f811def5cc0e3ce69c5641842
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-08 07:37:31 -0700

    begun laying out logic of game loop

--- app.rb
+++ app.rb
@@ -8,12 +8,27 @@ class Game
   COLORS = %w[Blue Red Orange Yellow Green Purple].freeze
 
   def initialize
+    @game_over = false
     @turn = 1
     @code = []
     4.times { @code.push(COLORS.sample) }
   end
 
+  private
+
   def increment_turn
     @turn += 1
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  public
+
+  def start_game
+    until game_over?
+
+    end
   end
 end
