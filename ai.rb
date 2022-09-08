0d40777fed1eabf8027cfe322682196b32698af4
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-08 15:31:50 -0700

    made foundation of ai class

---
+++ ai.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: false
+
+require './peg'
+
+# AI to make informed guesses
+class AI
+  OPTIONS = [Peg.new('B'), Peg.new('P'), Peg.new('O'), Peg.new('Y'), Peg.new('G'), Peg.new('R')].freeze
+
+  def initialize
+    @eliminated = [[], [], [], []]
+  end
+
+  def pick_peg(_eliminated)
+    OPTIONS.reject { |option| @eliminated.include?(option) }.sample
+  end
+end
